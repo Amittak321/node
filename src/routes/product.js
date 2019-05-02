91b585d2494fc730a8379b49f5d2161f995d92b4
Amittak321 <30614276+Amittak321@users.noreply.github.com>
2019-05-02 19:02:38 +0530

    Update product.js

--- src/routes/product.js
+++ src/routes/product.js
@@ -27,60 +27,57 @@ const upload = multer({
     }
 }).any()
 
-
 router.get('/', (req, res) => {
-      res.render('product.ejs')
+    res.render('product.ejs')
 });
 
 //select the product details form DB by id 
 router.get('/profile/:id', (req, res) => {
-      const message = "";
-      const id = req.params.id;
-      //finding the data by id
-      Product.findById(id)
-            .exec()
-            .then(doc => {
-                  res.render('profile.ejs', { data: doc, message: message });
-            })
-            .catch(err => console.log(err));
+    const message = "";
+    const id = req.params.id;
+    //finding the data by id
+    Product.findById(id)
+        .exec()
+        .then(doc => {
+            res.render('profile.ejs', { data: doc, message: message });
+        })
+        .catch(err => console.log(err));
 });
 
 //getting product info form web page
 router.post('/', urlencodedParser, (req, res) => {
-      console.log(req.files);
-      upload(req, res, function (err) {
-            if (err instanceof multer.MulterError) {
-                // A Multer error occurred when uploading.
-                console.log(err);
-            } else if (err) {
-                // An unknown error occurred when uploading.
-                console.log(err)
-            }
-             // Everything went fine.
-            if((req.files).length > 0){
-                  const product = new Product({
-                        _id: new mongoose.Types.ObjectId(),
-                        productName: req.body.product_name,
-                        image1: req.files[0].originalname,
-                        image2: req.files[1].originalname,
-                        image3: req.files[2].originalname,
-                        price: req.body.price,
-                        quantity: req.body.quantity
-                    })
-                    
-                    product
-                        .save()
-                        .then(result => {
-                              console.log(result);
-                              res.redirect('product/profile/' + result._id);
-                    
-                        })
-                        .catch(err => console.log(err));
-            }
-            else{
-                console.log("Empty array");
-            }
-      })
+    console.log(req.files);
+    upload(req, res, function (err) {
+        if (err instanceof multer.MulterError) {
+            // A Multer error occurred when uploading.
+            console.log(err);
+        } else if (err) {
+            // An unknown error occurred when uploading.
+            console.log(err)
+        }
+        // Everything went fine.
+        if ((req.files).length > 0) {
+            const product = new Product({
+                _id: new mongoose.Types.ObjectId(),
+                productName: req.body.product_name,
+                image1: req.files[0].originalname,
+                image2: req.files[1].originalname,
+                image3: req.files[2].originalname,
+                price: req.body.price,
+                quantity: req.body.quantity
+            })
+            product
+                .save()
+                .then(result => {
+                    console.log(result);
+                    res.redirect('product/profile/' + result._id);
+                })
+                .catch(err => console.log(err));
+        }
+        else {
+            console.log("Empty array");
+        }
+    })
 });
 
 module.exports = router;
